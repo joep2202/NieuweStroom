77ed54c168cf8d625b56d127e669bc4834195332
Joep <j.i.blanksma@student.utwente.nl>
2023-12-28 11:54:12 +0100

    V4

---
+++ Data_input/Data_retrieval.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+data = pd.read_csv("data/data.csv")
+print(data)
--- Data_input/main_data_input.py
+++ Data_input/main_data_input.py
@@ -1,34 +1,48 @@
 import PySimpleGUI as sg
 import pandas as pd
+import numpy as np
 import csv
 
 number_layouts = [1,2,3]
-appliances = ['batterij', 'EV laadpaal', 'Warmtepomp', 'Koelcel']
-size = (20, 1)
+appliances = ['Batterij', 'EV laadpaal', 'Warmtepomp', 'Koelcel', 'AC', 'Warm water boiler']
+type_flex = ['File', ['Gebruik', ['Limiteren van gebruik', 'Verschuiven van gebruik', ['Uitstellen', 'Naar voren schuiven', 'Beide']],
+                      'Opslag', ['Mobiel (EV)', 'Batterij', ['Met opwek', 'Zonder opwek']],
+                      ]]
+
+type_flex2 = ['Gebruik','Opslag']
+type_flex3 = ['Limiteren van gebruik', 'Verschuiven van gebruik']
+size = (35, 1)
 filled_in = True
 first = False
+gebruik = True
 data = pd.DataFrame()
-header_list = ['company_name','customer_id','Branche','KW','Remarks','appl','appl0','appl1','appl2']
+check = np.array([])
+value_type = ''
+#header_list = ['company_name','customer_id','Branche','KW','Remarks','appl','appl0','appl1','appl2']
+
+menu_def_2 = ['gas', 'mkb', 'industry']
 
 # ----------- Create the 3 layouts this Window will display -----------
-layout1 = [[sg.Text('Welcome to the data entry sheet for flex options')],
-            [sg.Text('Please enter your company', size =size), sg.Input(key='company_name')],
+layout1 = [[sg.Text('Welcome to the data entry sheet for flex options', background_color = 'darkblue')],
+            [sg.Text('Company name', size =size), sg.Input(key='company_name')],
             [sg.Text('Customer id', size =size), sg.Input(key='customer_id')],
-            [sg.Text('branche', size =size), sg.Input(key='Branche')],
-            [sg.Text('KW', size =size), sg.Input(key='KW')],
-            [sg.Text('Remarks', size =size), sg.Input(key='Remarks')],
-            [sg.Text('Welke type apparaat?')],
-            *[[sg.R(f'{appliances[i]}', 1, key="appl")] for i in range(len(appliances))]
+            [sg.Text('Branche', size =size), sg.Input(key='Branche')],
+            #[sg.Text('Branche', size=size), sg.OptionMenu(values=menu_def_2, key='Branche')],
+            [sg.Text('Needed information', background_color = 'darkblue')],
+            #[sg.Text('Type of appliance', size=size), sg.OptionMenu(values=appliances, key='Appliance')],
+            [sg.Text('Type of appliance (indien niet in lijst vul zelf in)', size=size), sg.Combo(values=appliances, key='Appliance')],
+            #[sg.Text('Indien overig', size =size), sg.Input(key='overig_type')],
+            [sg.Text('Maximale capaciteit in kW', size =size), sg.Input(key='max_KW')],
+            [sg.Text('Gemiddelde gebruikstijd in uur', size =size), sg.Input(key='duration')],
+            [sg.Text('Type flexibiliteit', size=size), sg.ButtonMenu('type', menu_def=type_flex, key='type_flex')],
+            [sg.Text('Piek aansluining', size =size), sg.Input(key='PiekAansluiting')],
+            [sg.Text('ICT info', background_color = 'darkblue')],
+            [sg.Text('IP address system', size =size), sg.Input(key='IP')],
+            [sg.Text('Access link backend appliance', size =size), sg.Input(key='ICT_APPL')],
+            [sg.Text('Access link smartmeter', size =size), sg.Input(key='ICT_METER')],
+            [sg.Text('Extra', background_color = 'darkblue')],
+            [sg.Text('Remarks', size =size), sg.Input(key='Remarks')]
            ]
-
-# layout3 = [[sg.Text('Welke apparaten heb je?')],
-#            *[[sg.CB(f' {appliances[i]}')] for i in range(4)]]
-
-layout3 = [[sg.Text('Welke type apparaat?')],
-           *[[sg.R(f'{appliances[i]}', 1)] for i in range(len(appliances))]]
-
-# layout4 = [[sg.Text('Submit your application')],
-#            [sg.Button('Previous'), sg.Button('Submit')]]
 
 # ----------- Create actual layout using Columns and a row of Buttons
 layout = [[sg.Column(layout1, key='-COL1-')],
@@ -52,22 +66,25 @@ layout = 1  # The currently visible layout
 while True:
     event, values = window.read()
     if event == 'Submit':
+        # if filled_check == True:
+        #     sg.popup_ok("Check if you filled in all the fields correctly")
+        #     filled_check = False
+        # elif filled_check == False:
+        #     retrive_data(event, values)
+        #     break
         for i, x in enumerate(values):
             # print(i, x)
             # print(values[x])
             if values[x] == '':
                 filled_in = False
         if filled_in == True:
-            #print(event, values)
+            # print(event, values)
             retrive_data(event, values)
             break
         elif filled_in == False:
-            print('I need a pop up to say fill in everything')
+            sg.popup_ok("You need to fill in all the fields")
         filled_in = True
     elif event in (None, 'Exit') or event == sg.WIN_CLOSED:
         break
-
-
-
 
 window.close()
